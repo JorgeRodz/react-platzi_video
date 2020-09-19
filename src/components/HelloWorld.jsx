2a45f466ad139e3296a08151fbaabe7549de41f4
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2020-09-19 14:19:18 -0500

    Creado el primer componente HelloWorld.jsx

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React from 'react' // para importar los elementos desde react
+
+// Utilizamos un <div> si queremos poner multiples elementos
+const HelloWorld = () => {
+  return (
+    <div> 
+      <h1>Hello world</h1>
+      <h2>Me llamo Jorge</h2>
+    </div>
+  )
+}
+
+export default HelloWorld // Exportamos HelloWorld para poder utilizarlo en el index.js y a su vez en el ReactDOM
