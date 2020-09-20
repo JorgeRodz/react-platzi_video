3f3f3be1aeb8f7188ff72f10b42c5c398b0e4d94
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2020-09-20 16:48:58 -0500

    Agregamos las configuraciones necesarias al archivo webpack.config.js para trabajar con las depencias/plugin de css-sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path') // para hacer uso del modulo path
 const HtmlWebpackPlugin = require('html-webpack-plugin') // instanciamos el plugin que instalamos
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // instanciamos el plugin para trabajar con css-sass
 
 module.exports = {
   entry: './src/index.js', // punto de entrada sera nuestro archivo principal index.js
@@ -15,22 +16,39 @@ module.exports = {
       { // para js y jsx
         test: /\.(js|jsx)/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
       },
       { // para html
         test: /\.html$/,
-        use: {
-          loader: 'html-loader'
-        }
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      { // para trabajar css con sass
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
-  plugins: [ // para utilizar el plugin que instalamos
+  plugins: [ // para utilizar los plugins que instalamos
     new HtmlWebpackPlugin({
       template: './public/index.html', // nuestro template de html
       filename: './index.html' // el nombre del archivo resultante
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css'
     })
   ]
 }
