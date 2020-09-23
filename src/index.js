ffbff6235642562213ab261d02f19b4d19ce1e7b
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2020-09-23 16:00:14 -0500

    Agregado el componente App a nuestro index.js para renderizarlo con el ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom' // para trabajar con el ReactDOM
-import HelloWorld from './components/HelloWorld' // importamos nuetro componente HelloWorld
+import React from 'react';
+import ReactDOM from 'react-dom'; // para trabajar con el ReactDOM
+import App from './containers/App';
 
 // Aqui van todos los componentes a renderizar
 ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById("app") // para colocar todos los componentes en nuestro index.html
-)
+  <App />,
+  document.getElementById('app'), // para colocar todos los componentes en nuestro index.html
+);
