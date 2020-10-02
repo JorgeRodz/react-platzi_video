6eae9e0c02b19e3cc1bd3ad1906146a30c24bfa8
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2020-10-02 15:29:17 -0500

    difinimos nuestros props en el componente CarouselItem.jsx

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarouselItem = ({ cover, title, year, contentRating, duration }) => {
+const CarouselItem = ({ cover, title, year, contentRating, duration, language }) => {
   return (
     <div className="carousel-item">
       <img className="carousel-item__img" src={cover} alt={title} />
@@ -14,9 +15,19 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => {
         </div>
         <p className="carousel-item__details--title">{title}</p>
         <p className="carousel-item__details--subtitle">{`${year} ${contentRating} ${duration}`}</p>
+        <p className="carousel-item__details--subtitle">{`${language}`}</p>
       </div>
     </div>
   );
+};
+
+CarouselItem.propTypes = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+  language: PropTypes.string,
 };
 
 export default CarouselItem;
