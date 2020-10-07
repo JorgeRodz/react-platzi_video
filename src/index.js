e11e592da8bcb7df68c9488fe9bd41743c16e00b
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2020-10-07 18:32:27 -0500

    Agregamos un Provider perteneciente a react-redux. Agregamos createStore perteneciente a redux. Estof fueron agregados en src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; // para trabajar con el ReactDOM
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
 
 // Aqui van todos los componentes a renderizar
 ReactDOM.render(
-  <App />,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('app'), // para colocar todos los componentes en nuestro index.html
 );
