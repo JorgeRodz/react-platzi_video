8362ac2a21dbc05ddff9e182425d18be40dd1edb
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2020-10-13 14:43:14 -0500

    Importamos connect de react-redux asi como tambien los actions para hacer usos de estos en el componente CarouselItem.jsx

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,13 +1,18 @@
+/* eslint-disable react/jsx-curly-newline */
+/* eslint-disable operator-linebreak */
+/* eslint-disable indent */
+/* eslint-disable react/jsx-wrap-multilines */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux'; // para conectar nuestra aplicacion con los elementos de redux
-import { setFavorite } from '../actions'; // importamos la funcion setFavorite de nuestros actions
+import { setFavorite, deleteFavorite } from '../actions'; // importamos la funcion setFavorite de nuestros actions
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
+import removeIcon from '../assets/static/remove-icon.png';
 
 const CarouselItem = (props) => {
-  const { id, cover, title, year, contentRating, duration, language } = props;
+  const { id, cover, title, year, contentRating, duration, language, isMyList } = props;
 
   //para pasarle los datos a nuestro setFavorite
   const handleSetFavorite = () => {
@@ -20,17 +25,41 @@ const CarouselItem = (props) => {
     alert('Le diste play al boton de play');
   };
 
+  const handleDeleteFavorite = (itemId) => {
+    props.deleteFavorite(itemId);
+  };
+
   return (
     <div className="carousel-item">
       <img className="carousel-item__img" src={cover} alt={title} />
       <div className="carousel-item__details">
         <div>
-          <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" onClick={handlePlayIcon} />
-          <img className="carousel-item__details--img" src={plusIcon} alt="Plus Icon" onClick={handleSetFavorite} />
+          <img
+            className="carousel-item__details--img"
+            src={playIcon}
+            alt="Play Icon"
+            onClick={handlePlayIcon}
+          />
+          {
+            isMyList ?
+              <img
+                className="carousel-item__details--img"
+                src={removeIcon}
+                alt="Remove Icon"
+                onClick={() => handleDeleteFavorite(id)}
+              />
+            :
+              <img
+                className="carousel-item__details--img"
+                src={plusIcon}
+                alt="Plus Icon"
+                onClick={handleSetFavorite}
+              />
+          }
         </div>
         <p className="carousel-item__details--title">{title}</p>
         <p className="carousel-item__details--subtitle">{`${year} ${contentRating} ${duration}`}</p>
-        <p className="carousel-item__details--subtitle">{`${language}`}</p>
+        <p className="carousel-item__details--language">{`${language}`}</p>
       </div>
     </div>
   );
@@ -47,6 +76,7 @@ CarouselItem.propTypes = {
 
 const mapDispatchToProps = {
   setFavorite, // la funcion de los actions
+  deleteFavorite,
 };
 
 export default connect(null, mapDispatchToProps)(CarouselItem);
