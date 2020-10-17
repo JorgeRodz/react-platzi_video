d824d36b7a132e2c27adf6c97f78119601adc4eb
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2020-10-17 18:13:35 -0500

    Agregamos redux al componente Search.jsx para mostrar la informacion del usuario

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import '../assets/styles/components/Search.scss';
 
-const Search = () => {
+const Search = ({ user }) => {
+  function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+  };
+  console.log(user);
   return (
     <section className="main">
+      {isEmpty(user) || (
+        <h1 className="main__user">{user.email}</h1>
+      )}
       <h2 className="main__title">¿Qué quieres ver hoy?</h2>
       <input type="text" className="input" placeholder="Buscar..." />
     </section>
   );
 };
 
-export default Search;
+// mapStateToProps obtenemos el state de nuestras props y con esto ya podemos usar los elementos que se encuentren dentro de esta; en este caso nos interesa el objeto user.
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps, null)(Search);
